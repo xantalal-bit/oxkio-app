0ad426b14bf4b4e09d427d7a866ccfadfa16a711
xantalal-bit <xantalal@gmail.com>
2026-04-26 09:57:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,18 @@ const fs = require("fs");
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-const ROOT = __dirname;
 
-const server = http.createServer((req, res) => {
-  let filePath = req.url === "/" ? "/index.html" : req.url;
-  filePath = path.join(ROOT, filePath.split("?")[0]);
-
-  fs.readFile(filePath, (err, data) => {
+http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
     if (err) {
-      res.writeHead(404);
-      res.end("Not found");
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Server error");
       return;
     }
 
-    res.writeHead(200);
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
   });
-});
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Oxkio running on port ${PORT}`);
+}).listen(PORT, "0.0.0.0", () => {
+  console.log("Oxkio running on port " + PORT);
 });
